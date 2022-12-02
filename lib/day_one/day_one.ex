2fd3e657f1eba27b78a78cfc2f9397135aa74f8f
Wesley Fredrickson <wes@atomicfi.com>
2022-12-02 10:04:25 -0700

    day one

---
+++ lib/day_one/day_one.ex
@@ -0,0 +1,37 @@
+alias AdventOfCode.DayOne.Input
+
+defmodule AdventOfCode.DayOne do
+  def get_cals do
+    args = Input.input()
+
+    args
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn elf -> String.split(elf, "\n", trim: true) end)
+    |> Enum.map(fn elf ->
+      Enum.reduce(elf, 0, fn ration, elf_acc ->
+        {ration, _} = Integer.parse(ration)
+        elf_acc + ration
+      end)
+    end)
+    |> Enum.sort()
+  end
+
+  def part_one do
+    get_cals()
+    |> Enum.at(-1)
+  end
+
+  def part_two do
+    get_cals()
+    |> Enum.take(-3)
+    |> Enum.reduce(0, fn elf, acc -> elf + acc end)
+  end
+
+  def go do
+    part_one = part_one()
+
+    part_two = part_two()
+
+    %{part_one: part_one, part_two: part_two}
+  end
+end
