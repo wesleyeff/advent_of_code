27d257a33a44bba707029a6ae478f039a13bf30d
Wesley Fredrickson <wes@atomicfi.com>
2022-12-05 18:26:44 -0700

    day 03: redo prioritzer

--- lib/day_03/day_03.ex
+++ lib/day_03/day_03.ex
@@ -54,43 +54,16 @@ defmodule AdventOfCode.Day03 do
   end
 
   defp get_priority([char]) do
-    priority_map = %{
-      "a" => 1,
-      "b" => 2,
-      "c" => 3,
-      "d" => 4,
-      "e" => 5,
-      "f" => 6,
-      "g" => 7,
-      "h" => 8,
-      "i" => 9,
-      "j" => 10,
-      "k" => 11,
-      "l" => 12,
-      "m" => 13,
-      "n" => 14,
-      "o" => 15,
-      "p" => 16,
-      "q" => 17,
-      "r" => 18,
-      "s" => 19,
-      "t" => 20,
-      "u" => 21,
-      "v" => 22,
-      "w" => 23,
-      "x" => 24,
-      "y" => 25,
-      "z" => 26
-    }
+    char
+    |> :binary.first()
+    |> get_priority()
+  end
 
-    lower_char = String.downcase(char)
+  defp get_priority(char) when char in ?a..?z do
+    char - 96
+  end
 
-    priority = Map.get(priority_map, lower_char)
-
-    if char == lower_char do
-      priority
-    else
-      priority + 26
-    end
+  defp get_priority(char) when char in ?A..?Z do
+    char - 64 + 26
   end
 end
